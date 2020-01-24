73d9cd03ee3a1f7f77e9c9cce9ba3980d8f1cb9d
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2020-01-24 06:12:47 +0300

    Added email and username validations

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,15 @@
 class Question < ApplicationRecord
+  # Эта команда добавляет связь с моделью User на уровне объектов, она же
+  # добавляет метод .user к данному объекту.
+  #
+  # Когда мы вызываем метод user у экземпляра класса Question, рельсы
+  # поймут это как просьбу найти в базе объект класса User со значением id,
+  # который равен question.user_id.
+  belongs_to :user
+
+  # валидируем сразу и связь, теперь нельзя создать вопрос, у которого нет юзера
+  validates :user, :text, presence: true
+  validates :text, length: {maximum: 255}
+  # Все коллбэки
 end
+
